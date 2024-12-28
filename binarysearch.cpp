211274a095e9f60c1912f125fca788387273ec50
theramiway <ramyarajeshrao1411@gmail.com>
2024-12-29 00:27:03 +0530

    Create binarysearch.cpp

---
+++ binarysearch.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+
+using namespace std;
+
+
+int binarySearch(int arr[], int size, int key) {
+    int low = 0;
+    int high = size - 1;
+
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+
+        if (arr[mid] == key) {
+            return mid;
+        }
+        if (arr[mid] < key) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+    return -1;
+}
+
+
+void bubbleSort(int arr[], int size) {
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    int size;
+
+
+    cout << "Enter the number of elements: ";
+    cin >> size;
+
+    int arr[size];
+
+    cout << "Enter the array elements: ";
+    for (int i = 0; i < size; i++) {
+        cin >> arr[i];
+    }
+
+
+    bubbleSort(arr, size);
+    cout << "Sorted array elements: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+
+    int key;
+    cout << "Enter the element to search for: ";
+    cin >> key;
+
+    int result = binarySearch(arr, size, key);
+
+
+    if (result != -1) {
+        cout << "Element found at index " << result << endl;
+    } else {
+        cout << "Element not found" << endl;
+    }
+
+    return 0;
+}
