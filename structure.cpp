c0e70a113d823ca5decfb51fe9ff6e2784829bac
theramiway <ramyarajeshrao1411@gmail.com>
2024-12-29 11:31:56 +0530

    Update structure.cpp

--- structure.cpp
+++ structure.cpp
@@ -8,12 +8,22 @@ struct Student {
 };
 
 int main() {
-    Student s;
-    cout << "Enter the name: ";
-    cin.ignore();
-    cin.getline(s.sname, 30);
-    cout << "Enter the roll no and CGPA: ";
-    cin >> s.rno >> s.cgpa;
-    cout << "Name: " << s.sname << "\t Roll No: " << s.rno << "\t CGPA: " << s.cgpa << endl;
+    int n;
+    cout << "Enter the number of students: ";
+    cin >> n;
+    Student students[50]; 
+
+    for(int i = 0; i < n; i++) {
+        cout << "Enter the name of student " << i + 1 << ": ";
+        cin.ignore();
+        cin.getline(students[i].sname, 30);
+        cout << "Enter the roll no and CGPA of student " << i + 1 << ": ";
+        cin >> students[i].rno >> students[i].cgpa;
+    }
+
+    for(int i = 0; i < n; i++) {
+        cout << "Name: " << students[i].sname << "\t Roll No: " << students[i].rno << "\t CGPA: " << students[i].cgpa << endl;
+    }
+
     return 0;
 }
